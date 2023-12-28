bf220ef38ba9d2aae1bcdfe56d15ef37cdaa16c3
wincharm <15828720226@163.com>
2023-12-28 11:27:15 +0800

    数据预处理003

--- traindata.py
+++ traindata.py
@@ -9,20 +9,39 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 Image.MAX_IMAGE_PIXELS = None
 
 
-traindata_path = "../toUser/train/"
+train_path = "../toUser/train/"
 train_imgs = []
 train_masks = []
-for i in range(1, 3):
-    train_img = Image.open(traindata_path + f"img{i}/img{i}.tif")
-    train_mask = Image.open(traindata_path + f"img{i}/img{i}_mask.tif")  # img1_mask.tif 文件的mask前面多了一个空格
-    train_imgs.append(train_img)
-    train_masks.append(train_mask)
 
-# img1: [22019, 11369]  img2: [18164, 24004]
-
-# 旋转img2并裁剪
-img2 = train_imgs[1].rotate(angle=13)
-# 先在Image格式下裁剪，再转为tensor
+train_img = Image.open(train_path + "img1/img1.tif")
+train_mask = Image.open(train_path + "img1/img1_mask.tif")  # img1_mask.tif 文件的mask前面多了一个空格
+train_imgs.append(train_img)
+train_masks.append(train_mask)
 
 
+# 旋转img2，裁掉黑边
+# img2 = train_imgs[1].rotate(angle=13)
+# img2_mask = train_masks[1].rotate(angle=13)
+# img2.save("../toUser/train/img2/img2_rotated.tif")
+# img2_mask.save("../toUser/train/img2/img2_mask_rotated.tif")
 
+
+# (left, upper, right, lower) = (2992.0, 1500.0, 15932.0, 22680.0)
+# img2 = Image.open(train_path + "img2/img2_rotated.tif")
+# img2_mask = Image.open(train_path + "img2/img2_mask_rotated.tif")
+# img2 = img2.crop((left, upper, right, lower))
+# img2_mask = img2_mask.crop((left, upper, right, lower))
+# img2.save(train_path + "img2/img2_rotated_cropped.tif")
+# img2_mask.save(train_path + "img2/img2_mask_rotated_cropped.tif")
+
+
+img2 = Image.open(train_path + "img2/img2_rotated_cropped.tif")
+img2_mask = Image.open(train_path + "img2/img2_mask_rotated_cropped.tif")
+train_imgs.append(img2)
+train_masks.append(img2_mask)
+
+
+# for i in range(0, 2):
+#     print(train_imgs[i].size)
+#     print(train_masks[i].size)
+# img1: (22019, 11369), img2: (12940, 21180)
