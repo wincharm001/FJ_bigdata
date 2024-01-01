d6c7bfc06b99a7f5002a1521dcacf792e811e7f5
wincharm <15828720226@163.com>
2024-01-01 11:44:23 +0800

    准备训练

--- masks2labels.py
+++ masks2labels.py
@@ -2,39 +2,42 @@ import os
 
 import cv2
 
+masks_dir = './temp/'
+labels_dir = './labels'
+def masks2labels(masks_dir, labels_dir):
+    for j in os.listdir(masks_dir):
+        image_path = os.path.join(masks_dir, j)
+        # load the binary mask and get its contours
+        mask = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+        _, mask = cv2.threshold(mask, 1, 255, cv2.THRESH_BINARY)
 
-input_dir = './tmp/masks'
-output_dir = './tmp/labels'
+        H, W = mask.shape
+        contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-for j in os.listdir(input_dir):
-    image_path = os.path.join(input_dir, j)
-    # load the binary mask and get its contours
-    mask = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-    _, mask = cv2.threshold(mask, 1, 255, cv2.THRESH_BINARY)
+        # convert the contours to polygons
+        polygons = []
+        for cnt in contours:
+            if cv2.contourArea(cnt) > 200:
+                polygon = []
+                for point in cnt:
+                    x, y = point[0]
+                    polygon.append(x / W)
+                    polygon.append(y / H)
+                polygons.append(polygon)
 
-    H, W = mask.shape
-    contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        # print the polygons
+        with open('{}.txt'.format(os.path.join(labels_dir, j)[:-4]), 'w') as f:
+            for polygon in polygons:
+                for p_, p in enumerate(polygon):
+                    if p_ == len(polygon) - 1:
+                        f.write('{}\n'.format(p))
+                    elif p_ == 0:
+                        f.write('0 {} '.format(p))
+                    else:
+                        f.write('{} '.format(p))
 
-    # convert the contours to polygons
-    polygons = []
-    for cnt in contours:
-        if cv2.contourArea(cnt) > 200:
-            polygon = []
-            for point in cnt:
-                x, y = point[0]
-                polygon.append(x / W)
-                polygon.append(y / H)
-            polygons.append(polygon)
+            f.close()
 
-    # print the polygons
-    with open('{}.txt'.format(os.path.join(output_dir, j)[:-4]), 'w') as f:
-        for polygon in polygons:
-            for p_, p in enumerate(polygon):
-                if p_ == len(polygon) - 1:
-                    f.write('{}\n'.format(p))
-                elif p_ == 0:
-                    f.write('0 {} '.format(p))
-                else:
-                    f.write('{} '.format(p))
 
-        f.close()
+def split():
+    # 获取train和val文件夹列表，下划线分割取数字，在labels中取出相应数字的labels
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
 
-model = YOLO("yolov8n.pt")
+model = YOLO("weight\yolov8n-seg.pt")
 
-model.train(data="config.yaml", epochs=100, imgsz=512)
+model.train(data="config.yaml", epochs=100, imgsz=640)
